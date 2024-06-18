c87126ee067a38bc237ee41eae162b33c68fa998
sorin-iordache <sorin.robert768@gmail.com>
2024-06-18 12:18:27 +0300

    Add Computer Choice

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+let choice = ["Rock", "Paper", "Scissors"];
+
+function getComputerChoice(choice) {
+  let randomIndex = Math.floor(Math.random() * choice.length);
+  return choice[randomIndex];
+}
+
+console.log(getComputerChoice(choice));
